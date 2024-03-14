e3f5e1a12b0702bf2dad5e443ba3daf46d78fa51
Neeraj Kumar <98191185+neerajpatel2505@users.noreply.github.com>
2024-03-14 18:23:21 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -7,8 +7,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index),
-    path('home', home,name="home"),
+    # path('', index),
+    path('', home,name="home"),
 ]
 
 
